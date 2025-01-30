e9cf96af62f3cdeb64cd19f35f8e917f32750527
Gabriel <gabrielsventura1995@gmail.com>
2025-01-30 20:37:58 -0300

    atualizando portifolio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    let navBtn = $('.nav-item');
+
+
+    let about = $('#sobre');
+    let projects = $('#projetos');
+    let contacts = $('#contacts');
+    
+    let scrolto = '';
+
+    $(navBtn).click(function(){
+
+        let btnId = $(this).attr('id');
+    })
+
+});
